536afc2a7d4ac1c8ac872ca658653c389d34a9e3
thinkbase <thinkbase@users.noreply.github.com>
2015-03-14 15:23:22 +0800

    Import docker-registry 0.9.1 ( https://github.com/docker/docker-registry/tree/0.9.1 )

---
+++ 01-init/02-docker-registry-0.9.1/docker_registry/server/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Docker Registry Contributors'
+__copyright__ = 'Copyright 2014 Docker'
+__credits__ = []
+
+__license__ = 'Apache 2.0'
+__version__ = '0.9.1'
+__maintainer__ = __author__
+__email__ = 'docker-dev@googlegroups.com'
+__status__ = 'Production'
+
+__title__ = 'docker-registry'
+__build__ = 0x000000
+
+__url__ = 'https://github.com/docker/docker-registry'
+__description__ = 'Registry server for Docker'
+__download__ = 'https://github.com/docker/docker-registry/archive/master.zip'
