3239ac39cdad19b6225a306169bcd6851945b0d6
thinkbase <thinkbase@users.noreply.github.com>
2015-03-19 02:09:30 +0800

    apt-get install -y

--- 09-Apps/01-PortableTrac/files/install.sh
+++ 09-Apps/01-PortableTrac/files/install.sh
@@ -6,14 +6,14 @@ set -x
 apt-get update
 
 # Install dependencies
-apt-get install sqlite3
-apt-get install build-essential
-apt-get install python python-setuptools python-genshi python-subversion
-apt-get install python-dev libsqlite3-dev
+apt-get install -y sqlite3
+apt-get install -y build-essential
+apt-get install -y python python-setuptools python-genshi python-subversion
+apt-get install -y python-dev libsqlite3-dev
 easy_install pysqlite
 
 # Install git and clone PortableTrac
-apt-get install git
+apt-get install -y git
 mkdir ~/github
 cd ~/github
 git clone https://github.com/thinkbase/PortableTrac
@@ -21,7 +21,7 @@ git clone https://github.com/thinkbase/PortableTrac
 # Install PortableTrac
 ~/github/PortableTrac/linux/install$ sudo ./install-PortableTrac.sh
 
-#apt-get install apache2 apache2-utils libapache2-mod-python
+#apt-get install -y apache2 apache2-utils libapache2-mod-python
 
 # deploy supervisor
 cp /tmp/files/supervisord.conf /etc/supervisor/conf.d/supervisord.conf
